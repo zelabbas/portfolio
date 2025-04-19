5b687168779362bc950a6bdbbc2bc4445093c1a1
zelabbas <zakariaelabbassi2002@gmail.com>
2025-04-19 11:33:03 +0100

    update nav-bar add links to sections

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -7,34 +7,46 @@ function Navbar() {
   return (
     <div className='flex justify-between items-center py-6'>
         <div className='flex flex-shrink-0 items-center m-8'>
-            <a href="/" aria-label='Home'>
+            <a className='transition-all duration-300 ease scale-100 sm:scale-150' href="/" aria-label='Home'>
             <img
                 src={logo}
                 alt="Logo"
-                className="w-12 h-12 rounded-full object-cover mx-2"/>
+                className="w-12 h-12 rounded-full object-cover mx-2 shadow-[0_0_20px_3px_#4245c4]"/>
             </a>
         </div>
 
+        <div className="hidden md:flex md:justify-between md:items-center space-x-6 text-white font-medium ">
+            <a href="#technologies" className="hover:text-white hover:shadow-[0_0_15px_3px_#4245c4] transition-all duration-300 border p-2 rounded-xl hover:scale-90">Technologies</a>
+            <a href="#projects" className="hover:text-white hover:shadow-[0_0_15px_3px_#4245c4] transition-all duration-300 border p-2 rounded-xl hover:scale-90">Projects</a>
+            <a href="#education" className="hover:text-white hover:shadow-[0_0_15px_3px_#4245c4] transition-all duration-300 border p-2 rounded-xl hover:scale-90">Education</a>
+            <a href="#contact"   className="px-4 py-2 rounded-xl border border-transparent bg-gradient-to-r from-indigo-500 to-blue-600 text-white font-medium shadow-sm hover:shadow-[0_0_12px_2px_#4245c4] hover:scale-90 transition-all duration-300 ease-in-out">Contact</a>
+        </div>
+
         <div className='m-8 flex flex-shrink-0 items-center'> 
-            <a href="https://www.linkedin.com/in/zakariaa-elabbassi-564b712b4"
-                target='blank'
-                rel='noopener noreferrer'
-                aria-label='LinkedIn'>
-                <FaLinkedin className='text-2xl text-white mx-2 hover:text-blue-500'/>
+
+            <a
+              className="transition-all duration-300 ease hover:scale-125"
+              href="https://www.linkedin.com/in/zakariaa-elabbassi-564b712b4"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn">
+                <FaLinkedin className="w-9 h-9 text-white mx-2 p-1 rounded-full transition-all duration-300 ease-in-out hover:shadow-[0_0_9px_2px_#4245c4]" />
             </a>
 
-            <a className='' href="https://github.com/zelabbas"
+
+            <a className='transition-all duration-300 ease hover:scale-125' href="https://github.com/zelabbas"
                 target='blank'
                 rel='noopener noreferrer'
                 aria-label='GitHub'>
-                <FaGithub className='text-2xl text-white mx-2  hover:text-blue-500'/>
+                <FaGithub className='w-9 h-9 text-white mx-2 p-1 rounded-full transition-all duration-300 ease-in-out hover:shadow-[0_0_9px_2px_#4245c4]'/>
             </a>
 
-            <a href="https://www.instagram.com/zelabbass"
+            <a  className='transition-all duration-300 ease hover:scale-125'
+                href="https://www.instagram.com/zelabbass"
                 target='blank'
                 rel='noopener noreferrer'
                 aria-label='Instagram'>
-                <FaInstagram className='text-2xl text-white mx-2 hover:text-blue-500'/>
+                <FaInstagram className='w-9 h-9 text-white mx-2 p-1 rounded-full transition-all duration-300 ease-in-out hover:shadow-[0_0_9px_2px_#4245c4]'/>
             </a>
         </div>
     </div>
