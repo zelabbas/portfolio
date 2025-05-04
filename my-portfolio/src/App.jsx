e14578ca55d79ea914e9c976caed0915c63b7e2a
zelabbas <zakariaelabbassi2002@gmail.com>
2025-05-04 22:16:43 +0100

    fix a bug when scrolling down

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -8,7 +8,7 @@ import ContactForm from "./utils/ContactForm";
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-stone-300 antialiased">
+    <div className="overflow-hidden text-stone-300 antialiased">
       <div className="fixed inset-0 -z-10">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#4245c4_100%)]"></div>
       </div>
