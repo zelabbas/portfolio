ecceaae3c99e10b6993eccbb4bbf1c6df40212c8
zelabbas <zakariaelabbassi2002@gmail.com>
2025-09-11 19:56:50 +0100

    remove commented-out CORS middleware from server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
-// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
